99f830cc84c86b44ac6a7ffbc85b8960d4a85f72
Oskar Jansson <oskja067@student.liu.se>
2021-05-02 14:17:12 +0200

    adds: login

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment as env } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({...env.auth,}),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// TODO(iambullsaw)(2021-05-02): following auth0 guide, at step: https://auth0.com/blog/complete-guide-to-angular-user-authentication/#Add-User-Authentication
